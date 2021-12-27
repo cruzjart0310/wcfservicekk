e0a46205e27049f46ba16d35629cf5e0b86c79b7
Armando Sanchez Sanchez <armando.desarrollador@hotmail.com>
2021-12-27 01:13:36 -0600

    ya funciona :(

--- WcfServiceKKreme/IService.cs
+++ WcfServiceKKreme/IService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
@@ -11,31 +12,33 @@ namespace WcfServiceKKreme
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService" in both code and config file together.
     [ServiceContract]
-    
+
     public interface IService
     {
         [OperationContract]
-        [WebInvoke(Method = "GET", UriTemplate = "users/{id}", ResponseFormat = WebMessageFormat.Json)]
-        ResponseBase<User> ShowUserById(int id);
+        [WebInvoke(Method = "GET", UriTemplate = "users/{id}", ResponseFormat = WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json)]
+        ResponseBase<User> ShowUserById(string id);
+
 
         [OperationContract]
-        [WebInvoke(Method = "GET", UriTemplate = "coupons", ResponseFormat = WebMessageFormat.Json)]
+        [WebInvoke(Method = "GET", UriTemplate = "coupons", ResponseFormat = WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json)]
+        [Description("Mensjae xd")]
         ResponseBase<Coupon> AllCopuon();
 
         [OperationContract]
         [WebInvoke(Method = "GET", UriTemplate = "coupons/{id}", ResponseFormat = WebMessageFormat.Json)]
-        ResponseBase<Coupon> ShowCopuonById(int id);
+        ResponseBase<Coupon> ShowCopuonById(string id);
 
         [OperationContract]
         [WebInvoke(Method = "POST", UriTemplate = "coupons", ResponseFormat = WebMessageFormat.Json)]
         ResponseBase<Coupon> StoreCoupon(Coupon coupon);
 
-        [OperationContract]
-        [WebInvoke(Method = "PUT", UriTemplate = "coupons/{id}", ResponseFormat = WebMessageFormat.Json)]
-        ResponseBase<Coupon> UpdateCoupon(Coupon coupon);
+        //[OperationContract]
+        //[WebInvoke(Method = "PUT", UriTemplate = "coupons/{id}", ResponseFormat = WebMessageFormat.Json)]
+        //ResponseBase<Coupon> UpdateCoupon(Coupon coupon);
 
-        [OperationContract]
-        [WebInvoke(Method = "DELETE", UriTemplate = "coupons/{id}", ResponseFormat = WebMessageFormat.Json)]
-        ResponseBase<Coupon> RemoveCopuon(Coupon coupon);
+        //[OperationContract]
+        //[WebInvoke(Method = "DELETE", UriTemplate = "coupons/{id}", ResponseFormat = WebMessageFormat.Json)]
+        //ResponseBase<Coupon> RemoveCopuon(Coupon coupon);
     }
 }
